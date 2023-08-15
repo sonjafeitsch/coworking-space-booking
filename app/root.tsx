5fe6390161fb60adabf63d633b6f26bdede4c705
Sonja Feitsch <feitsch@e2n.de>
2023-08-15 09:45:26 +0200

    adjust routing

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -8,9 +8,13 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import styles from "./tailwind.css";
-import { ErrorBoundary } from "./ErrorBoundary";
+import image from "../public/assets/images/nische_ganz_800w.png";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "Meine Nische – Buchung" }];
+};
 
 export default function App() {
   return (
@@ -22,11 +26,17 @@ export default function App() {
         <Links />
       </head>
       <body className="h-screen bg-meine-nische-background font-sans text-themed-base-text">
-        <Outlet />
+        <div className="min-h-screen grid grid-cols-2">
+          <img
+            alt="cover"
+            src={image}
+            className="hidden lg:block lg:object-cover lg:overflow-hidden lg:h-0 lg:w-0 lg:min-h-full lg:min-w-full"
+          />
+          <Outlet />
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
-        <ErrorBoundary />
       </body>
     </html>
   );
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,4 @@
-import { json, LoaderArgs, type V2_MetaFunction } from "@remix-run/node";
+import { LoaderArgs, json } from "@remix-run/node";
 import {
   Form,
   useLoaderData,
@@ -9,16 +9,10 @@ import { getEvents } from "~/models/events";
 import dayjs from "dayjs";
 import localizedFormat from "dayjs/plugin/localizedFormat";
 import utc from "dayjs/plugin/utc";
-import "dayjs/locale/de";
-import image from "../../public/assets/images/nische_ganz_800w.png";
 
 dayjs.extend(localizedFormat);
 dayjs.extend(utc);
 dayjs.locale("de");
-
-export const meta: V2_MetaFunction = () => {
-  return [{ title: "Meine Nische – Buchung" }];
-};
 
 export const loader = async ({ request }: LoaderArgs) => {
   const url = new URL(request.url);
@@ -35,6 +29,7 @@ export const loader = async ({ request }: LoaderArgs) => {
     return json({ events });
   } catch (error) {
     // throw error;
+    return json({ events: [] });
   }
 };
 
@@ -69,67 +64,60 @@ export default function Index() {
   const { state } = useNavigation();
 
   return (
-    <div className="min-h-screen grid grid-cols-2">
-      <img
-        alt="cover"
-        src={image}
-        className="hidden lg:block lg:object-cover lg:overflow-hidden lg:h-0 lg:w-0 lg:min-h-full lg:min-w-full"
-      />
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-col justify-start w-1/2 gap-8">
-          <div className="lg:max-w-lg">
-            <h1 className="mt-2 text-3xl font-bold tracking-tight text-themed-text font-serif sm:text-4xl">
-              Meine Buchung
-            </h1>
-            <p className="mt-6 text-base leading-6 text-themed-base-text">
-              Gib den Wunschzeitraum für Deine Veranstaltung ein. Prüfe mit
-              einem Klick ob sie verfügbar ist.
-            </p>
-          </div>
-          <Form className="flex flex-col items-start gap-4">
-            <div className="flex gap-4">
-              <div>
-                <label
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  htmlFor="start"
-                >
-                  Von
-                </label>
-                <input
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  id="start"
-                  name="start"
-                  type="datetime-local"
-                  defaultValue={start}
-                />
-              </div>
-              <div>
-                <label
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                  htmlFor="end"
-                >
-                  Bis
-                </label>
-                <input
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                  id="end"
-                  name="end"
-                  type="datetime-local"
-                  defaultValue={end}
-                />
-              </div>
-            </div>
-            <button
-              className="border-bg-primary border text-white w-full bg-bg-primary hover:border-bg-primary hover:text-bg-primary hover:bg-white focus:ring-4 font-medium rounded-3xl text-sm px-5 py-2.5 mr-2 mb-2 focus:ring-transparent focus:outline-none"
-              type="submit"
-            >
-              {state === "loading"
-                ? "Events werden gesucht…"
-                : "Verfügbarkeit prüfen"}
-            </button>
-          </Form>
-          <Events start={start} end={end} />
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-start w-1/2 gap-8">
+        <div className="lg:max-w-lg">
+          <h1 className="mt-2 text-3xl font-bold tracking-tight text-themed-text font-serif sm:text-4xl">
+            Meine Buchung
+          </h1>
+          <p className="mt-6 text-base leading-6 text-themed-base-text">
+            Gib den Wunschzeitraum für Deine Veranstaltung ein. Prüfe mit einem
+            Klick ob sie verfügbar ist.
+          </p>
         </div>
+        <Form className="flex flex-col items-start gap-4">
+          <div className="flex gap-4">
+            <div>
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="start"
+              >
+                Von
+              </label>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                id="start"
+                name="start"
+                type="datetime-local"
+                defaultValue={start}
+              />
+            </div>
+            <div>
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="end"
+              >
+                Bis
+              </label>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                id="end"
+                name="end"
+                type="datetime-local"
+                defaultValue={end}
+              />
+            </div>
+          </div>
+          <button
+            className="border-bg-primary border text-white w-full bg-bg-primary hover:border-bg-primary hover:text-bg-primary hover:bg-white focus:ring-4 font-medium rounded-3xl text-sm px-5 py-2.5 mr-2 mb-2 focus:ring-transparent focus:outline-none"
+            type="submit"
+          >
+            {state === "loading"
+              ? "Events werden gesucht…"
+              : "Verfügbarkeit prüfen"}
+          </button>
+        </Form>
+        <Events start={start} end={end} />
       </div>
     </div>
   );
